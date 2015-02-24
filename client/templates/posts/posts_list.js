ce3570ec6cb546ada93573d9030736fd4c054e04
austinsamsel <austinsamsel@gmail.com>
2015-02-23 20:18:56 -0500

    add basic posts list template and static data.

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,17 @@
+var postsData = [
+  {
+    title: 'proposal for klast'
+  },
+  {
+    title: 'proposal for davler'
+  },
+  {
+    title: 'proposal for bkfp'
+  },
+  {
+    title: 'proposal for creators'
+  },
+];
+Template.postsList.helpers({
+  posts: postsData
+});
