3f6cfb8a3c65485bd820bc6a0dd072da3598271b
austinsamsel <austinsamsel@gmail.com>
2015-02-26 17:52:06 -0500

    add submit post page and linked to it in the header.

---
+++ client/templates/posts/post_submit.js
@@ -0,0 +1,13 @@
+Template.postSubmit.events({
+  'submit form': function(e) {
+    e.preventDefault();
+
+    var post = {
+      title: $(e.target).find('[name=title]').val(),
+      content: $(e.target).find('[name=content]').val()
+    };
+
+    post._id = Posts.insert(post);
+    Router.go('postPage', post);
+  }
+});
