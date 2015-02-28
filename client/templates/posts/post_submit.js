ea311faf2939351dde1d5b5c26d2f0c940c0704e
austinsamsel <austinsamsel@gmail.com>
2015-02-28 12:03:53 -0500

    validate post contents on submission

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,3 +1,15 @@
+Template.postSubmit.created = function() {
+  Session.set('postSubmitErrors', {});
+}
+Template.postSubmit.helpers({
+  errorMessage: function(field) {
+    return Session.get('postSubmitErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+  }
+});
+
 Template.postSubmit.events({
   'submit form': function(e) {
     e.preventDefault();
@@ -6,6 +18,10 @@ Template.postSubmit.events({
       title: $(e.target).find('[name=title]').val(),
       content: $(e.target).find('[name=content]').val()
     };
+
+    var errors = validatePost(post);
+    if (errors.title || errors.content)
+      return Session.set('postSubmitErrors', errors);
 
     Meteor.call('postInsert', post, function(error, result) {
       // display the error to the user and abort
