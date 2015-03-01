4add408cdccf8ab2fefb3b16688b5c141b8a3115
austinsamsel <austinsamsel@gmail.com>
2015-03-01 17:13:50 -0500

    add signatures collection, pub/sub and fixtures

--- server/fixtures.js
+++ server/fixtures.js
@@ -18,8 +18,7 @@ if (Posts.find().count() === 0) {
     author: paris.profile.name,
     content: 'a contract here.',
     submitted: new Date(now - 7 * 3600 * 1000),
-    commentsCount: 2,
-    accepted: 'CEO BOOM.'
+    commentsCount: 2
   });
 
   Comments.insert({
@@ -38,6 +37,22 @@ if (Posts.find().count() === 0) {
     body: 'Wow looks great. Awesome.'
   });
 
+  Signatures.insert({
+    postId: porposalId,
+    userId: austo._id,
+    author: austo.profile.name,
+    submitted: new Date(now - 5 * 3600 * 1000),
+    body: 'Signed ~ ceo'
+  });
+
+  Signatures.insert({
+    postId: porposalId,
+    userId: paris._id,
+    author: paris.profile.name,
+    submitted: new Date(now - 3 * 3600 * 1000),
+    body: 'BOOM ~capitan'
+  });
+
   Posts.insert({
     title: 'BKFP',
     expiration: new Date(now - 10 * 3600 * 1000),
@@ -45,8 +60,7 @@ if (Posts.find().count() === 0) {
     author: austo.profile.name,
     content: 'anotehr contract',
     submitted: new Date(now - 10 * 3600 * 1000),
-    commentsCount: 0,
-    accepted: null,
+    commentsCount: 0
   });
 
   Posts.insert({
@@ -56,8 +70,7 @@ if (Posts.find().count() === 0) {
     author: austo.profile.name,
     content: 'sup. a proposal and a contract',
     submitted: new Date(now - 12 * 3600 * 1000),
-    CommentsCount: 0,
-    accepted: null,
+    CommentsCount: 0
   });
 
   for (var i = 0; i < 10; i++) {
@@ -68,8 +81,7 @@ if (Posts.find().count() === 0) {
       userId: paris._id,
       content: 'sample proposal and contract' + i,
       submitted: new Date(now - i * 3600 * 1000),
-      commentsCount: 0,
-      accepted: null,
+      commentsCount: 0
     });
   }
 }
--- server/publications.js
+++ server/publications.js
@@ -19,3 +19,8 @@ Meteor.publish('comments', function(postId) {
 Meteor.publish('notifications', function() {
   return Notifications.find({userId: this.userId, read: false});
 });
+
+Meteor.publish('signatures', function(postId) {
+  check(postId, String);
+  return Signatures.find({postId: postId});
+});
