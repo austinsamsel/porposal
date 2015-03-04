272cd6d6232468a9b391fa8d8c21b44fc67db6f9
austinsamsel <austinsamsel@gmail.com>
2015-03-03 23:28:33 -0500

    denormalized the status of signatures on posts

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,13 +1,5 @@
 Template.postItem.helpers({
   ownPost: function() {
     return this.userId === Meteor.userId();
-  },
-  signatureStatus: function() {
-    var pending = Signatures.find({postId: this._id}).count();
-    if (pending === 0) {
-      return "pending"
-    } else {
-      return "approved"
-    };
   }
 });
